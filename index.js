7a73844db33e73c66d98fe083aca37b5b1a8df73
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-09 22:06:38 +0200

    vyřešeno i s bonusy

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Funguju')
+const kameny = document.querySelectorAll(".kamen")
+
+kameny.forEach((kamen) => {
+    kamen.addEventListener("click", () => {
+        document.querySelector("h1").textContent = kamen.textContent
+        new Audio(`tony/${kamen.textContent}.mp3`).play()    
+    })
+})
+
+document.addEventListener("keydown", (e) => {
+    if(e.code.startsWith("Digit")) {
+        const koncovka = e.code.slice(e.code.length - 1)
+        document.querySelector("h1").textContent = kameny[koncovka-1].textContent
+        new Audio(`tony/${kameny[koncovka-1].textContent}.mp3`).play()
+    }
+})
